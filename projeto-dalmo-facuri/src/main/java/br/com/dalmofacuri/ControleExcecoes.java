694cb3ae2d1d5026f374b93f659ac7d8b7372e83
dalmo <mffacuri@terra.com.br>
2023-10-09 16:26:40 -0300

    Controller Advice p2: Tratamento da maioria das excecoes na classe ControleExcecoes.

--- projeto-dalmo-facuri/src/main/java/br/com/dalmofacuri/ControleExcecoes.java
+++ projeto-dalmo-facuri/src/main/java/br/com/dalmofacuri/ControleExcecoes.java
@@ -1,6 +1,7 @@
 package br.com.dalmofacuri;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.postgresql.util.PSQLException;
@@ -8,6 +9,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,9 +27,20 @@ public class ControleExcecoes extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({ Exception.class, RuntimeException.class, Throwable.class })
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
+		
+		    String msg = "";
+		
+		if(ex instanceof MethodArgumentNotValidException) {
+			 List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+			  for (ObjectError objectError : list) {
+				  msg += objectError.getDefaultMessage() + "\n";
+			}
+		}else {
+			msg = ex.getMessage();
+		}
 
 		ObjetoError objetoError = new ObjetoError();
-		objetoError.setError(ex.getMessage());
+		objetoError.setError(msg);
 		objetoError.setCode(status.value() + " ==>" + status.getReasonPhrase());
 		return new ResponseEntity<>(objetoError, headers, status);
 	}
